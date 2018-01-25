0eba782a16d387c46a5667f9f5ac913f93781f1c
kimcoleman <kim@strangerstudios.com>
2018-01-25 10:43:27 -0500

    Moving tabs javascript back into theme for use in comments / pinkbacks

--- js/memberlite-shortcodes.js
+++ js/memberlite-shortcodes.js
@@ -1,34 +1,23 @@
 /**
  * memberlite-shortcodes.js
  */
-jQuery( document ).ready(function() {
-  //switch tab content when clicked
-  jQuery('.memberlite_tabbable .memberlite_tabs li a').click(function(e) {
-  
-	//don't want to jump to #
-	e.preventDefault();
-	
-	//which tab was clicked
-	var tab, tabarea;
-	tab = jQuery(this).attr('href').replace(/#/, '');
-	tabarea = jQuery(this).closest('.memberlite_tabbable');
-	
-	//hide all tab panes
-	tabarea.find('.memberlite_tab_pane').hide();
-	tabarea.find('.memberlite_tab_pane').removeClass('memberlite_active');
-	
-	//show the active one
-	jQuery('#' + tab).show();
-	jQuery('#' + tab).addClass('memberlite_active');
-	
-	//unstyle tabs
-	tabarea.find('.memberlite_tabs li').removeClass('memberlite_active');
-	
-	//highlight the active one
-	jQuery(this).closest('li').addClass('memberlite_active');
-  
-  });
-  
-  //check if we should switch tab content on page loads
-  jQuery('a[href="' + window.location.hash + '"]').click();
+jQuery( document ).ready( function() {
+
+	//show accordion content when clicked
+	jQuery('.memberlite_accordion-item h3').click(function(e) {
+		
+		var accordion_trigger, accordion, accordion_wrapper;
+
+		accordion_trigger = jQuery(this);
+		accordion = accordion_trigger.parent();
+		accordion_wrapper = accordion.parent();
+
+		accordion_trigger.next().slideToggle(350);
+		accordion_wrapper.addClass('memberlite-active');
+
+		if(accordion_wrapper.hasClass('memberlite_accordion-collapse')) {
+			accordion_wrapper.addClass('kim');
+		}
+	});	 
+
 });
