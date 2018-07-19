848d33b4fa29308fde6fbd281ce1440d9afd60bc
Kimberly Coleman <kim@strangerstudios.com>
2018-07-19 13:34:30 -0400

    Updating to use Font Awesome 5.1.1; added "type" attribute to icon shortcode.

--- shortcodes/font-awesome.php
+++ shortcodes/font-awesome.php
@@ -2,14 +2,37 @@
 function memberlitesc_fa_shortcode($atts, $content = null) {
 	// $atts    ::= array of attributes
 	// $content ::= text within enclosing form of shortcode element
-	// examples: [fa icon="comment" color="primary" size="3x"]
+	// examples: [fa icon="comment" color="primary" type="solid" size="3x"]
 
     extract(shortcode_atts(array(
 		'color' => '',
 		'icon' => '',
 		'size' => '',
+        'type' => '',
     ), $atts));
-    $r = '<i class="fa fa-' . $icon;
+    $r = '<i class="';
+
+    $font_awesome_icons_brands = memberlite_shortcodes_get_font_awesome_icons( 'brand' );
+
+    // Check if the icon is a "brand" icon and set the type attribute.
+    if ( in_array( $icon, $font_awesome_icons_brands ) ) {
+        $type = 'brand';
+    }
+
+    if ( ! empty( $type ) ) {
+        if ( $type === 'regular' ) {
+            $r .= 'far';
+        } elseif ( $type === 'solid' ) {
+            $r .= 'fas';
+        } elseif ( $type === 'brand' ) {
+            $r .= 'fab';
+        }
+	} else {
+        $r .= 'fa';
+    }
+
+    $r .= ' fa-' . $icon;
+
 	if(!empty($color))
 	{
 		$r .= ' ' . $color;
