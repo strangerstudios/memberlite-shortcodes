51b03c5f6df55baa863e7d57decacecc9bdcb865
Kimberly Coleman <kim@strangerstudios.com>
2018-08-01 09:32:11 -0400

    Fixing display of Recent Posts shortcode to reflect an improved grid layout

--- shortcodes/recent_posts.php
+++ shortcodes/recent_posts.php
@@ -6,12 +6,12 @@
  */
 function memberlitesc_recent_posts_shortcode_handler($atts, $content=null, $code="") {
 	global $post;
-	
+
 	// $atts    ::= array of attributes
 	// $content ::= text within enclosing form of shortcode element
 	// $code    ::= the shortcode found, when == callback name
 	// examples: [memberlite_recent_posts title="Recent Posts" show="none" category_id="2"]
-	
+
 	extract(shortcode_atts(array(
 		'post_type' => 'post',
 		'author_id' => NULL,
@@ -30,7 +30,7 @@ function memberlitesc_recent_posts_shortcode_handler($atts, $content=null, $code
 		'ignore_sticky_posts' => true,
 		'author' => $author_id,
 	);
-	
+
 	// check to see if post type is not empty and the post type exists.
 	if( !empty($post_type) && post_type_exists( $post_type ) ) {
 		$query_args['post_type'] = $post_type;
@@ -52,7 +52,7 @@ function memberlitesc_recent_posts_shortcode_handler($atts, $content=null, $code
 			return _e( 'There was a problem fetching posts for the current user. Please try again later.', 'memberlite-shortcodes' );
 		}else{
 			$query_args['author'] = $current_user->ID;
-		}	
+		}
 	}
 
 	// If author_id is not numeric, try and get the user's ID from their name.
@@ -69,28 +69,28 @@ function memberlitesc_recent_posts_shortcode_handler($atts, $content=null, $code
 	}
 
 	// our return string
-	$r = '<div class="memberlite_recent_posts">';
-		
+	$r = '<div id="widget_memberlite_recent_posts" class="memberlite_recent_posts">';
+
 	// get posts
 	if( !empty( $category_id ) ) {
 		$query_args['cat'] = $category_id;
 	}
 
 	// allows users to filter the post query per post.
-	$query_args = apply_filters( 'memberlite_recents_posts_query', $query_args, $post );	
+	$query_args = apply_filters( 'memberlite_recents_posts_query', $query_args, $post );
 
 	// Run the post query.
 	query_posts( $query_args );
-	
+
 	if(!empty($title))
 		$r .= '<h1>' . $title . '</h1>';
 	if(!empty($subtitle))
 		$r .= '<p class="subtitle">' . $subtitle . '</p>';
-	
+
 	$r .= '<div class="row">';
-	
+
 	$counter = 0;
-	
+
 	if($count == "1")
 		$colclass = "large-12";
 	elseif($count == "2")
@@ -101,44 +101,38 @@ function memberlitesc_recent_posts_shortcode_handler($atts, $content=null, $code
 		$colclass = "medium-3 small-12";
 	else
 		$colclass = "medium-6";
-	
-	// the Loop		
-	if ( have_posts() ) : while ( have_posts() ) : the_post();	
+
+	// the Loop
+	if ( have_posts() ) : while ( have_posts() ) : the_post();
 		$counter++;
 
 		$r .= '<div class="' . $colclass . ' columns ';
-		if ( has_post_thumbnail() || !empty($show_avatar) )
-		{
+		if ( has_post_thumbnail() || ! empty( $show_avatar ) ) {
 			$r .= ' widget_has_thumbnail';
 		}
 		$r .= '">';
 
 		$r .= '<article id="post-' . get_the_ID() . '" class="' . implode(" ", get_post_class()) . '">';
 
-		if ( has_post_thumbnail() || !empty($show_avatar) ) 
-		{
-			if ( has_post_thumbnail() )
-			{
-				$r .= '<a class="widget_post_thumbnail" href="' . get_permalink() . '">' . get_the_post_thumbnail($post->ID, 'mini') . '</a>';
-			}
-			else
-			{
+		$r .= '<header class="entry-header">';
+
+		if ( has_post_thumbnail() || ! empty( $show_avatar ) ) {
+			if ( has_post_thumbnail() ) {
+				$r .= '<a class="widget_post_thumbnail" href="' . get_permalink() . '">' . get_the_post_thumbnail($post->ID, 'thumbnail') . '</a>';
+			} else {
 				$author_id = get_the_author_meta( 'ID' );
 				$r .= '<a class="widget_post_thumbnail" href="' . get_permalink() . '">' . get_avatar( $author_id, 80 ) . '</a>';
 			}
 		}
-		elseif( 'video' == get_post_format() )
-			$r .= '<a class="widget_post_thumbnail" href="' . get_permalink() . '"><i class="fa fa-video-camera"></i></a>';
-		
-		$r .= '<header class="entry-header">';
+
 		$r .= '<h4 class="entry-title">';
 		$r .= '<a href="' . get_permalink() . '" rel="bookmark">';
 		$r .= the_title('','',false);
 		$r .= '</a>';
 		$r .= '</h4>';
 		$r .= '<span class="post-date">' . get_the_date() . '</span>';
-		
-		$r .= '</header>';		
+
+		$r .= '</header>';
 
 		$r .= '<div class="entry-content">';
 		if($show == "excerpt")
@@ -148,21 +142,21 @@ function memberlitesc_recent_posts_shortcode_handler($atts, $content=null, $code
 		$r .= '</div>';
 		$r .= '</article>';
 		$r .= '</div>';
-		
+
 		if(($count > 4) && ($counter % 2 == 0))
 		{
 			$r .= '</div><!-- .row -->';
 			$r .= '<div class="row">';
 		}
-		
+
 	endwhile; endif;
-	
+
 	if(! (($count > 4) && ($counter % 2 == 0)) )
 	{
 		$r .= '</div><!-- .row -->';
 		$r .= '</div> <!-- .memberlite_recent_posts -->';
 	}
-	
+
 	//Reset Query
 	wp_reset_query();
 
