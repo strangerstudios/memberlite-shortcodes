66a30acebf209068e12682422ef706fa12edd24c
Jason Coleman <jason@strangerstudios.com>
2020-09-11 11:43:35 +1000

    doc blocks and moving stuff around

--- shortcodes/tabs.php
+++ shortcodes/tabs.php
@@ -1,5 +1,15 @@
 <?php
-// Tabs Content Wrapper
+/**
+ * The memberlite_tabs shortcode.
+ * This shortcode is a wrapper for each
+ * individual membership_tab shortcode.
+ *
+ * Example:
+ * [memberlite_tabs items="Item 1,Item 2"]
+ * [memberlite_tab item="Item 1"]Item 1 tab content.[/memberlite_tab]
+ * [memberlite_tab item="Item 2"]Item 2 tab content.[/memberlite_tab]
+ * [/memberlite_tabs]
+ */
 function memberlitesc_tabs_shortcode($atts, $content = null) {
 	// $atts    ::= array of attributes
 	// $content ::= text within enclosing form of shortcode element
@@ -33,6 +43,34 @@ function memberlitesc_tabs_shortcode($atts, $content = null) {
 }
 remove_shortcode('memberlite_tabs');	//replace shortcode bundled with Memberlite 2.0 and prior or anywhere else
 add_shortcode('memberlite_tabs', 'memberlitesc_tabs_shortcode');
+
+/**
+ * The memberlite_tab shortcode.
+ * These shortcodes should be inside of a memberlite_tabs shortcode.
+ *
+ * Example:
+ * [memberlite_tab class="tab1" item="Tab 1"]Tab content.[/memberlite_tab]
+ */
+function memberlitesc_tab_shortcode($atts, $content = null) {
+	// $atts    ::= array of attributes
+	// $content ::= text within enclosing form of shortcode element
+	// examples: [memberlite_tab class="text-center" item="Tab 1"]
+    extract(shortcode_atts(array(
+		'class' => '',
+		'item' => '',
+    ), $atts));
+	$memberlite_active_tabs = memberlitesc_check_active_tab( $item );
+	$item_id = sanitize_title_with_dashes( $item );
+    $result = '<div class="memberlite_tab_pane ' . $class;
+	if( in_array( $item, $memberlite_active_tabs ) )
+		$result .= ' memberlite_active';
+	$result .= '" id="tab-' . $item_id . '" >';
+    $result .= do_shortcode($content);
+    $result .= '</div>';
+    return force_balance_tags($result);
+}
+add_shortcode('memberlite_tab', 'memberlitesc_tab_shortcode');
+
 function memberlitesc_check_active_tab( $items = array() ){
 	global $post;
 	$memberlite_active_tabs = array();
@@ -57,22 +95,3 @@ function memberlitesc_check_active_tab( $items = array() ){
 	}
 	return $memberlite_active_tabs;
 }
-function memberlitesc_tab_shortcode($atts, $content = null) {
-	// $atts    ::= array of attributes
-	// $content ::= text within enclosing form of shortcode element
-	// examples: [memberlite_tab class="text-center" item="Tab 1"]
-    extract(shortcode_atts(array(
-		'class' => '',
-		'item' => '',
-    ), $atts));
-	$memberlite_active_tabs = memberlitesc_check_active_tab( $item );
-	$item_id = sanitize_title_with_dashes( $item );
-    $result = '<div class="memberlite_tab_pane ' . $class;
-	if( in_array( $item, $memberlite_active_tabs ) )
-		$result .= ' memberlite_active';
-	$result .= '" id="tab-' . $item_id . '" >';
-    $result .= do_shortcode($content);
-    $result .= '</div>';
-    return force_balance_tags($result);
-}
-add_shortcode('memberlite_tab', 'memberlitesc_tab_shortcode');
